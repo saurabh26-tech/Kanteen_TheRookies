3a0b8e3fec613dcd3d87a3d21a41bc36b5172dbf
Saurabh Pandey <saurabhpandey267@gmail.com>
2020-11-20 22:55:13 +0530

    splash

--- app/src/main/java/com/TheRookies/kanteen/Splash.java
+++ app/src/main/java/com/TheRookies/kanteen/Splash.java
@@ -2,7 +2,9 @@ package com.TheRookies.kanteen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        }, 4000);
     }
 }
